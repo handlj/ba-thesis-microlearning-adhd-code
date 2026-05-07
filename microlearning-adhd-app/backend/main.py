0143afeed35e56d0fd4f34c5ffbca3a8cc6e9d06
Jan Simon Handler <jan.handler@student.tugraz.at>
2026-05-07 11:19:20 +0200

    really add backend

---
+++ microlearning-adhd-app/backend/main.py
@@ -0,0 +1,50 @@
+# -------------------------------------
+# Microlearning ADHD App - Backend
+# -------------------------------------
+# This is the main entry point for the FastAPI backend server.
+# 
+# In its current state, it is only a mock up, following the 
+# tutorial at https://www.youtube.com/watch?v=aSdVU9-SxH4 to 
+# set up the basic structure.
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+
+class Items(BaseModel):
+    items: List[Item]
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+memory_items = {"items": []}
+
+@app.get("/items", response_model=Items)
+def get_items():
+    return Items(items=memory_items["items"])
+
+@app.post("/items", response_model=Items)
+def add_item(item: Item):
+    memory_items["items"].append(item)
+    return Items(items=memory_items["items"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
